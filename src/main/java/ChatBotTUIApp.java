16d02a89c198de8c903ea4feea603650b0defbaf
Алина <@DESKTOP-306NSDC>
2021-11-15 14:10:05 +0200

    working

--- src/main/java/ChatBotTUIApp.java
+++ src/main/java/ChatBotTUIApp.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class ChatBotTUIApp {
 
-	public static void main(String[] args) {
-		System.out.println("Ласкаво просимо до CHATBOT");
-		String question;
-		boolean askAQuestion = true;
-		Scanner keyboard = new Scanner(System.in);
-		Bot bot = new Bot();
-		do {
-			System.out
-					.println("Скажи щось ChatBot (введі enter До побачення щоб вийти): ");
-			question = keyboard.nextLine();
-			if (question.equalsIgnoreCase("до побачення")
-					|| question.equalsIgnoreCase("допобачення")
-					|| question.equalsIgnoreCase("бувай")
-					|| question.equalsIgnoreCase("пока") {
-				askAQuestion = false;
-			} else
-				System.out.println("Бот каже: \"" + bot.ask(question) + "\"");
-		} while (askAQuestion);
-		keyboard.close();
-		System.out.println("Бот каже: \"До побачення!\"");
-	}
+    public static void main(String[] args) {
+        System.out.println("Ласкаво просимо до CHATBOT");
+        String question;
+        boolean askAQuestion = true;
+        Scanner keyboard = new Scanner(System.in);
+        Bot bot = new Bot();
+        do {
+            System.out
+                    .println("Скажи щось ChatBot (введі enter До побачення щоб вийти): ");
+            question = keyboard.nextLine();
+            if (question.equalsIgnoreCase("до побачення")
+                    || question.equalsIgnoreCase("допобачення")
+                    || question.equalsIgnoreCase("бувай")
+                    || question.equalsIgnoreCase("пока")){
+                askAQuestion = false;
+            } else
+            System.out.println("Бот каже: \"" + bot.ask(question) + "\"");
+        } while (askAQuestion);
+        keyboard.close();
+        System.out.println("Бот каже: \"До побачення!\"");
+    }
 }
